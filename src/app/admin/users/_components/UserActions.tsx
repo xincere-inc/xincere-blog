1f5fca64f53dec3bc8df8bf23c2a90276fee2b42
hasan1119 <rokibul.devs@gmail.com>
2025-04-08 05:54:52 +0600

    Refactor: code

---
+++ src/app/admin/users/_components/UserActions.tsx
@@ -0,0 +1,31 @@
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+import { Button, Popconfirm, Space } from 'antd';
+
+interface UserActionsProps {
+  record: any;
+  loggedInUserId?: string;
+  onEdit: (record: any) => void;
+  onDelete: (record: any) => void;
+}
+
+export function UserActions({ record, loggedInUserId, onEdit, onDelete }: UserActionsProps) {
+  return (
+    <Space size="middle">
+      <Button type="primary" icon={<EditOutlined />} onClick={() => onEdit(record)}>
+        Edit
+      </Button>
+      {record.id !== loggedInUserId && (
+        <Popconfirm
+          title="Are you sure to delete this user?"
+          onConfirm={() => onDelete(record)}
+          okText="Yes"
+          cancelText="No"
+        >
+          <Button type="primary" danger icon={<DeleteOutlined />}>
+            Delete
+          </Button>
+        </Popconfirm>
+      )}
+    </Space>
+  );
+}
---
+++ src/app/admin/users/_components/UserCreateModal.tsx
@@ -0,0 +1,138 @@
+import { Button, Col, Form, Input, Modal, Row, Select } from 'antd';
+const { Option } = Select;
+
+interface UserCreateModalProps {
+  visible: boolean;
+  onCancel: () => void;
+  onCreate: (values: any) => void;
+  loading: boolean;
+}
+
+export function UserCreateModal({ visible, onCancel, onCreate, loading }: UserCreateModalProps) {
+  const [form] = Form.useForm();
+
+  return (
+    <Modal
+      title="Create User"
+      open={visible}
+      onCancel={() => {
+        onCancel();
+        form.resetFields();
+      }}
+      footer={null}
+      destroyOnClose
+      centered
+      style={{ margin: '20px 0px' }}
+    >
+      <Form form={form} onFinish={onCreate} layout="vertical">
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="First Name"
+              name="firstName"
+              rules={[{ required: true, message: 'Please input the first name!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Last Name"
+              name="lastName"
+              rules={[{ required: true, message: 'Please input the last name!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                { required: true, message: 'Please input the email!' },
+                { type: 'email', message: 'Invalid email format!' },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true, message: 'Please input the username!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Phone"
+              name="phone"
+              rules={[{ required: true, message: 'Please input the phone number!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Country"
+              name="country"
+              rules={[{ required: true, message: 'Please input the country!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item
+              label="Address"
+              name="address"
+              rules={[{ required: true, message: 'Please input the address!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Role"
+              name="role"
+              rules={[{ required: true, message: 'Please select the role!' }]}
+            >
+              <Select>
+                <Option value="user">User</Option>
+                <Option value="admin">Admin</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: 'Please input the password!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Create User
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+}
