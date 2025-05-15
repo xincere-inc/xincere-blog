8f6dac4b348b4bf0d7047acfa03c803c5d325490
hasan1119 <rokibul.devs@gmail.com>
2025-05-15 16:11:03 +0600

    fix: footer problems

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,13 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faGithub,
-  faLinkedin,
-  faFacebook,
-  faTwitter,
-} from '@fortawesome/free-brands-svg-icons';
+import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 
 const Footer = () => {
-  const iconStyle: React.CSSProperties = { padding: 10, fontSize: 24 };
-
   return (
     <footer className="bg-gray-800 text-white py-10 mt-12">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
-            <h4 className="text-xl font-bold mb-4">SINCERE Tech Blog</h4>
+            <h4 className="text-xl font-bold mb-4">Xincere Blog</h4>
             <p className="text-gray-300 text-sm">
               最新の技術トレンドやシステム開発事例、効果的な開発手法のノウハウを発信しています。
             </p>
@@ -64,25 +56,25 @@ const Footer = () => {
                 href="#"
                 className="text-gray-300 hover:text-white transition-colors duration-300 cursor-pointer"
               >
-                <FontAwesomeIcon style={iconStyle} icon={faTwitter} />
+                <FaXTwitter fontSize={24} />
               </a>
               <a
                 href="#"
                 className="text-gray-300 hover:text-white transition-colors duration-300 cursor-pointer"
               >
-                <FontAwesomeIcon style={iconStyle} icon={faFacebook} />
+                <FaFacebook fontSize={24} />
               </a>
               <a
                 href="#"
                 className="text-gray-300 hover:text-white transition-colors duration-300 cursor-pointer"
               >
-                <FontAwesomeIcon style={iconStyle} icon={faLinkedin} />
+                <FaLinkedin fontSize={24} />
               </a>
               <a
                 href="#"
                 className="text-gray-300 hover:text-white transition-colors duration-300 cursor-pointer"
               >
-                <FontAwesomeIcon style={iconStyle} icon={faGithub} />
+                <FaGithub fontSize={24} />
               </a>
             </div>
           </div>
