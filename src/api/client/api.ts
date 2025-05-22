ad9e8796131c886a1364b48ca6e08638a2cf9c08
jasonmag <jasonmaglangit@gmail.com>
2025-05-22 22:42:30 +0800

    added interface for getArticles

--- src/api/client/api.ts
+++ src/api/client/api.ts
@@ -85,6 +85,58 @@ export interface AdminDeleteArticlesRequest {
 /**
  *
  * @export
+ * @interface AdminGetArticlesRequest
+ */
+export interface AdminGetArticlesRequest {
+  page: number;
+  limit: number;
+  search?: string;
+}
+
+/**
+ *
+ * @export
+ * @interface AdminArticlePreview
+ */
+export interface AdminArticlePreview {
+  id: number;
+  title: string;
+  slug: string;
+  summary: string;
+  thumbnailUrl?: string;
+  status: 'DRAFT' | 'PUBLISHED' | 'ARCHIVED';
+  createdAt: string;
+  updatedAt: string;
+  author: {
+    id: string;
+    name: string;
+    email: string;
+  };
+  category: {
+    id: number;
+    name: string;
+  };
+}
+
+/**
+ *
+ * @export
+ * @interface AdminGetArticles200Response
+ */
+export interface AdminGetArticles200Response {
+  data: AdminArticlePreview[];
+  pagination: {
+    page: number;
+    limit: number;
+    showPerPage: number;
+    totalArticles: number;
+    totalPages: number;
+  };
+}
+
+/**
+ *
+ * @export
  * @interface AdminCreateUser400Response
  */
 export interface AdminCreateUser400Response {
