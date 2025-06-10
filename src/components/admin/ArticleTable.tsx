e43c177afae7d831f3a55506cd76b442342b231c
jasonmag <jasonmaglangit@gmail.com>
2025-06-10 23:16:01 +0800

    added supporting info for article collections

--- src/components/admin/ArticleTable.tsx
+++ src/components/admin/ArticleTable.tsx
@@ -1,10 +1,12 @@
 'use client';
-import { AdminGetUsers200ResponseDataInner } from '@/api/client';
+import { AdminGetArticles200ResponseDataInner } from '@/api/client';
 import IdoAdminArticles from '@/api/IdoAdminArticles';
 import { Table } from 'antd';
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { ArticleSearchBar } from './ArticleSearchBar';
+import { ArticleSelection } from './ArticleSelection';
+import { ArticleActions } from './ArticleActions';
 
 
 export interface Article {
@@ -33,6 +35,7 @@ export default function ArticleTable() {
   const [loading, setLoading] = useState(false)
   const [isCreateModalVisible, setIsCreateModalVisible] = useState(false);
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+  const [article, setArticle] = useState<Article | null>(null);
 
   const fetchData = async (page: number, pageSize: number, search: string) => {
     setLoading(true);
@@ -43,6 +46,29 @@ export default function ArticleTable() {
         search,
       });
       console.log('get article:', response);
+      
+      if (response.status === 200) {
+        const articles = 
+          response.data?.data?.map(
+            (article: AdminGetArticles200ResponseDataInner) => ({
+              ...article,
+              id: article.id || '',
+              author: article.author?.name || '',
+              title: article.title || '',
+              category: article.category?.name || '',
+              slug: article.slug || '',
+              summary: article.summary || '',
+              status: article.status || '',
+              tags: article.tags || ''
+            })
+          ) || [];
+
+        setData(articles);
+        setPagination((prev) => ({
+          ...prev,
+          total: response.data?.pagination?.totalArticles || 0,
+        }));
+      }
     } catch (err: any) {
       toast.error(err.response?.data.message || 'Something went wrong.', {
         position: 'bottom-right',
@@ -64,13 +90,61 @@ export default function ArticleTable() {
     setPagination((prev) => ({ ...prev, current: 1 }));
   };
 
+  const handleTableChange = (pagination: any) => {
+    setPagination(pagination);
+    fetchData(pagination.current, pagination.pageSize, searchText);
+  };
+
+  const handleEdit = (record: Article) => {
+    setArticle({ ...record});
+    // setIsEditModalVisible(true);
+  };
+
+  const handleDelete = (record: Article) => {
+    if(record.id) {
+      deleteArticle([record.id.toString()]);
+    }
+  };
+
+  const columns = [
+    {
+      title: 'Select',
+      key: 'select',
+      render: (_: any, record: Article) => (
+        <ArticleSelection
+          record={record}
+          selectedRowKeys={selectedRowKeys}
+          onSelectChange={handleSearchChange}
+        />
+      ),
+    },
+    { title: 'Title', dataIndex: 'title', key: 'title' },
+    { title: 'Author', dataIndex: 'author', key: 'author' },
+    { title: 'Summary', dataIndex: 'summary', key: 'summary' },
+    { title: 'Category', dataIndex: 'category', key: 'category' },
+    { title: 'Slug', dataIndex: 'slug', key: 'slug' },
+    { title: 'Status', dataIndex: 'status', key: 'status' },
+    { title: 'Tags', dataIndex: 'tags', key: 'tags' },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_: any, record: Article) => (
+        <ArticleActions
+          record={record}
+          onEdit={handleEdit}
+          onDelete={handleDelete}
+        />
+      ),
+    },
+  ];
+
   const handleBulkDelete = () => {
-      if (selectedRowKeys.length > 0) {
-        deleteArticle(selectedRowKeys as string[]);
-      } else {
-        toast.error('No article selected.', { position: 'bottom-right' });
-      }
-    };
+    if (selectedRowKeys.length > 0) {
+      deleteArticle(selectedRowKeys as string[]);
+    } else {
+      toast.error('No article selected.', { position: 'bottom-right' });
+    }
+  };
 
   return (
     <div className="p-4">
@@ -81,7 +155,17 @@ export default function ArticleTable() {
         onBulkDeleteClick={handleBulkDelete}
         selectedRowCount={selectedRowKeys.length}
       />
-      <Table/>
+      <Table
+        columns={columns}
+        dataSource={data}
+        rowKey="id"
+        pagination={pagination}
+        loading={loading}
+        onChange={handleTableChange}
+        scroll={{ x: true }}
+        style={{ width: '100%' }}
+      />
+
     </div>
   );
 }
