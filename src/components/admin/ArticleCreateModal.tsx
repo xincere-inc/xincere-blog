5922c18d3a62979ced640400e1a213fa3df8ec32
jasonmag <jasonmaglangit@gmail.com>
2025-06-12 23:34:39 +0800

    updated article create modal

--- src/components/admin/ArticleCreateModal.tsx
+++ src/components/admin/ArticleCreateModal.tsx
@@ -1,6 +1,5 @@
-import {Alert, Button, Col, Form, FormInstance, Input, Modal, Row, Select} from 'antd';
+import {Alert, Button, Col, Form, FormInstance, Input, Modal, Row} from 'antd';
 import { useImperativeHandle } from 'react';
-const { Option } = Select;
 
 interface ArticleCreateModalProps {
   visible: boolean;
@@ -23,7 +22,100 @@ export function ArticleCreateModal({
   useImperativeHandle(formRef, () => form);
 
   return (
-    <Modal></Modal>
+    <Modal
+      title="Create Article"
+      open={visible}
+      onCancel={() => {
+        onCancel();
+        form.resetFields();
+      }}
+      footer={null}
+      destroyOnClose
+      centered
+      style={{ margin: '20px 0px' }}
+    >
+      <Form form={form} onFinish={onCreate} layout="vertical">
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Title"
+              name="title"
+              rules={[
+                { required: true, message: 'Please input Article Title!' },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Author"
+              name="author"
+              rules={[
+                { required: true, message: 'Please input Author!' },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Category"
+              name="category"
+              rules={[
+                { required: true, message: 'Please input the category' },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col span={24}>
+            <Form.Item label="Slug" name="slug">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Row gutter={16}>
+          <Col xs={24} sm={12}>
+            <Form.Item
+              label="Summary"
+              name="summary"
+            >
+            </Form.Item>
+          </Col>
+        </Row>
+
+        <Button
+          className="form-btn"
+          type="primary"
+          htmlType="submit"
+          loading={loading}
+        >
+          Create Article
+        </Button>
+        <Row>
+          <Col span={24}>
+            {serverError && (
+              <Alert
+                message={serverError}
+                type="error"
+                style={{ marginTop: '10px' }}
+              />
+            )}
+          </Col>
+        </Row>
+      </Form>
+    </Modal>
   );
 
 }
