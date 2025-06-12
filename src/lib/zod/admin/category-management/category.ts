f264e98638e94b38f3c20c62047acda0877ec4c2
Md Rokibul Hasan <rokibul.devs@gmail.com>
2025-06-12 11:46:34 +0600

    chore: shared fields for schema separated

--- src/lib/zod/admin/category-management/category.ts
+++ src/lib/zod/admin/category-management/category.ts
@@ -1,6 +1,13 @@
 import { z } from 'zod';
 import { descriptionField, nameField, slugField } from './categoryFields';
 
+// Define the Zod schema for creating a new category by admin
+export const adminCreateCategorySchema = z.object({
+  name: nameField,
+  slug: slugField,
+  description: descriptionField,
+});
+
 // Define the Zod schema for updating a category by admin
 export const adminUpdateCategorySchema = z.object({
   id: z.number().int().positive('ID must be a positive integer'),
