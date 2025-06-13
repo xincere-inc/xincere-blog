26a40806ec0cdbad7d3f1c8b7837ec0ea6d88853
Md Rokibul Hasan <rokibul.devs@gmail.com>
2025-06-13 12:12:36 +0600

    updated code retrive from the feature/48

--- src/api/client/api.ts
+++ src/api/client/api.ts
@@ -507,6 +507,56 @@ export interface AdminGetUsersRequest {
 /**
  * 
  * @export
+ * @interface AdminUpdateCategory200Response
+ */
+export interface AdminUpdateCategory200Response {
+    /**
+     * 
+     * @type {string}
+     * @memberof AdminUpdateCategory200Response
+     */
+    'message'?: string;
+    /**
+     * 
+     * @type {AdminCreateCategory201ResponseCategory}
+     * @memberof AdminUpdateCategory200Response
+     */
+    'category'?: AdminCreateCategory201ResponseCategory;
+}
+/**
+ * 
+ * @export
+ * @interface AdminUpdateCategoryRequest
+ */
+export interface AdminUpdateCategoryRequest {
+    /**
+     * The ID of the category to update.
+     * @type {number}
+     * @memberof AdminUpdateCategoryRequest
+     */
+    'id': number;
+    /**
+     * The name of the category.
+     * @type {string}
+     * @memberof AdminUpdateCategoryRequest
+     */
+    'name'?: string;
+    /**
+     * The unique slug for the category.
+     * @type {string}
+     * @memberof AdminUpdateCategoryRequest
+     */
+    'slug'?: string;
+    /**
+     * The description of the category (optional).
+     * @type {string}
+     * @memberof AdminUpdateCategoryRequest
+     */
+    'description'?: string | null;
+}
+/**
+ * 
+ * @export
  * @interface AdminUpdateUser200Response
  */
 export interface AdminUpdateUser200Response {
@@ -1386,6 +1436,46 @@ export const AdminApiAxiosParamCreator = function (configuration?: Configuration
             };
         },
         /**
+         * Updates details of an existing category based on the provided category ID.
+         * @summary Update category details
+         * @param {AdminUpdateCategoryRequest} adminUpdateCategoryRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        adminUpdateCategory: async (adminUpdateCategoryRequest: AdminUpdateCategoryRequest, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'adminUpdateCategoryRequest' is not null or undefined
+            assertParamExists('adminUpdateCategory', 'adminUpdateCategoryRequest', adminUpdateCategoryRequest)
+            const localVarPath = `/api/admin/categories/update-category`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'PUT', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication jwt required
+            // http bearer authentication required
+            await setBearerAuthToObject(localVarHeaderParameter, configuration)
+
+
+    
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            localVarRequestOptions.data = serializeDataIfNeeded(adminUpdateCategoryRequest, localVarRequestOptions, configuration)
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+        /**
          * Update details of an existing user based on the provided user ID.
          * @summary Update user details
          * @param {AdminUpdateUserRequest} adminUpdateUserRequest 
@@ -1499,6 +1589,19 @@ export const AdminApiFp = function(configuration?: Configuration) {
             return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
         },
         /**
+         * Updates details of an existing category based on the provided category ID.
+         * @summary Update category details
+         * @param {AdminUpdateCategoryRequest} adminUpdateCategoryRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async adminUpdateCategory(adminUpdateCategoryRequest: AdminUpdateCategoryRequest, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<AdminUpdateCategory200Response>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.adminUpdateCategory(adminUpdateCategoryRequest, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['AdminApi.adminUpdateCategory']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+        /**
          * Update details of an existing user based on the provided user ID.
          * @summary Update user details
          * @param {AdminUpdateUserRequest} adminUpdateUserRequest 
@@ -1572,6 +1675,16 @@ export const AdminApiFactory = function (configuration?: Configuration, basePath
          */
         adminGetUsers(adminGetUsersRequest: AdminGetUsersRequest, options?: RawAxiosRequestConfig): AxiosPromise<AdminGetUsers200Response> {
             return localVarFp.adminGetUsers(adminGetUsersRequest, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * Updates details of an existing category based on the provided category ID.
+         * @summary Update category details
+         * @param {AdminUpdateCategoryRequest} adminUpdateCategoryRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        adminUpdateCategory(adminUpdateCategoryRequest: AdminUpdateCategoryRequest, options?: RawAxiosRequestConfig): AxiosPromise<AdminUpdateCategory200Response> {
+            return localVarFp.adminUpdateCategory(adminUpdateCategoryRequest, options).then((request) => request(axios, basePath));
         },
         /**
          * Update details of an existing user based on the provided user ID.
@@ -1653,6 +1766,18 @@ export class AdminApi extends BaseAPI {
      */
     public adminGetUsers(adminGetUsersRequest: AdminGetUsersRequest, options?: RawAxiosRequestConfig) {
         return AdminApiFp(this.configuration).adminGetUsers(adminGetUsersRequest, options).then((request) => request(this.axios, this.basePath));
+    }
+
+    /**
+     * Updates details of an existing category based on the provided category ID.
+     * @summary Update category details
+     * @param {AdminUpdateCategoryRequest} adminUpdateCategoryRequest 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof AdminApi
+     */
+    public adminUpdateCategory(adminUpdateCategoryRequest: AdminUpdateCategoryRequest, options?: RawAxiosRequestConfig) {
+        return AdminApiFp(this.configuration).adminUpdateCategory(adminUpdateCategoryRequest, options).then((request) => request(this.axios, this.basePath));
     }
 
     /**
